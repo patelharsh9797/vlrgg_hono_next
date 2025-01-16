96b2b58266f5f236f4590affa7db62bc910b91c2
Harsh Patel <patelharsh9797@gmail.com>
2025-01-16 06:38:57 +0000

    Added getBaseUrl fn

--- src/lib/hono-client.ts
+++ src/lib/hono-client.ts
@@ -1,4 +1,9 @@
 import { hc } from "hono/client";
 import type { HonoAppType } from "@/server";
+import { getBaseUrl } from "@/utils";
 
-export const api = hc<HonoAppType>("http://localhost:3000/api"); 
+const url = getBaseUrl() + "api";
+
+console.log("URL: ", url)
+
+export const api = hc<HonoAppType>(url); 
---
+++ src/utils/index.ts
@@ -0,0 +1,19 @@
+export const getBaseUrl = () => {
+    // browser should use relative path
+    if (typeof window !== "undefined") {
+        return ""
+    }
+
+    if (process.env.NODE_ENV === "development") {
+        return "http://localhost:3000/"
+    }
+
+    // if deployed to vercel, use vercel url
+    if (process.env.VERCEL_URL) {
+        return `https://${process.env.VERCEL_URL}`
+    }
+
+    // assume deployment to cloudflare workers otherwise, you'll get this URL after running
+    // `npm run deploy`, which deploys your server to cloudflare
+    return "https://<YOUR_DEPLOYED_WORKER_URL>/"
+}
